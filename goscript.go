5cb209080122a8fc4d1d2157577b877154e3dff1
APPLE <apple@APPLEs-MacBook-Pro.local>
2022-09-25 10:39:45 +0530

    functions added in lib

--- goscript.go
+++ goscript.go
@@ -16,11 +16,11 @@ func StringReverse(str string) (result string) {
 	return
 }
 
-func StringContains(str string, strToCompare string) (result bool) {
+func StringContainsChar(str string, strToCompare string) (result int) {
 	strArr := []rune(str)
 	for i := len(strArr) - 1; i >= 0; i-- {
 		if string(strArr[i]) == strToCompare {
-			return true
+			result++
 		}
 	}
 	return
@@ -77,7 +77,110 @@ func StringToSlice(str string) []string {
 	return result
 }
 
-func StringSplit(str string, splitter string) []string {
+func StringRemoveFirstChar(str string) string {
+	strArr := []rune(str)
+	result := ""
+	for i := 1; i <= len(strArr)-1; i++ {
+		result += string(strArr[i])
+	}
+
+	return result
+}
+
+func StringRemoveLastChar(str string) string {
+	strArr := []rune(str)
+	result := ""
+	for i := 0; i <= len(strArr)-1; i++ {
+		if i != len(strArr)-1 {
+			result += string(strArr[i])
+		}
+	}
+
+	return result
+}
+
+func StringCharInstances(str string, occurer string) (result int) {
+	if StringLength(occurer) > 1 {
+		return 0
+	}
+	strArr := []rune(str)
+	for i := 0; i <= len(strArr)-1; i++ {
+		if string(strArr[i]) == occurer {
+			result += 1
+		}
+	}
+
+	return
+}
+
+func IntSliceHas(s []int, index int) (result bool) {
+	for _, v := range s {
+		if v == index {
+			result = true
+		}
+	}
+	return
+}
+
+func StringRemove(str string, occurer string) (result string) {
+	myresult := []int{}
+	strarr := StringToSlice(str)
+	occurerresult := StringToSlice(occurer)
+	for i, v := range strarr {
+		for _, vx := range occurerresult {
+			if v == vx {
+				myresult = append(myresult, i)
+			}
+		}
+	}
+	for i, v := range strarr {
+		if !IntSliceHas(myresult, i) {
+			result += v
+		}
+
+	}
+	return
+}
+
+func StringContains(str string, occurer string) (result int) {
+	strArr := []rune(str)
+	myresult := []string{}
+	p := ""
+	for i := 0; i <= len(strArr)-1; i++ {
+		p += string(strArr[i])
+		if StringLength(p) > StringLength((occurer)) {
+			p = StringRemoveFirstChar(p)
+		}
+		if StringLength(p) == StringLength((occurer)) && p == occurer {
+			myresult = append(myresult, p)
+			result++
+		}
+	}
+	return
+}
+
+func StringSplit(str string, occurer string) (myresult []string) {
+	strArr := []rune(str)
+	// myresult := []string{}
+	p := ""
+	t := ""
+	for i := 0; i <= len(strArr)-1; i++ {
+		p += string(strArr[i])
+		t += string(strArr[i])
+		if StringLength(p) > StringLength((occurer)) {
+			p = StringRemoveFirstChar(p)
+		}
+		if StringLength(p) == StringLength((occurer)) && p == occurer {
+			myresult = append(myresult, StringRemove(t, occurer))
+			t = ""
+			// result++
+		}
+	}
+	myresult = append(myresult, StringRemove(t, occurer))
+	return
+}
+
+func StringSplitWithChar(str string, splitter string) []string {
 	// str += splitter
 	strArr := []rune(str)
 	myresult := []string{}
